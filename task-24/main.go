eea1526fb42aae7c9765471c5a486fcc1178b84a
Akhmed Borshchigov <akhmed9505@gmail.com>
2025-12-19 21:52:52 +0300

    Решение l1.24 Расстояние между точками

---
+++ task-24/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+Разработать программу нахождения расстояния между двумя точками на плоскости.
+Точки представлены в виде структуры Point с инкапсулированными (приватными) полями x, y (типа float64) и конструктором.
+Расстояние рассчитывается по формуле между координатами двух точек.
+Подсказка: используйте функцию-конструктор NewPoint(x, y), Point и метод Distance(other Point) float64.
+*/
+
+type Point struct {
+	x, y float64
+}
+
+func NewPoint(x, y float64) *Point {
+	return &Point{x, y}
+}
+
+func (p *Point) Distance(other *Point) float64 {
+	dx := p.x - other.x
+	dy := p.y - other.y
+	return math.Sqrt(dx*dx + dy*dy)
+}
+
+func main() {
+	p1 := NewPoint(1, 2)
+	p2 := NewPoint(3, 4)
+
+	fmt.Printf("Расстояние между точками: %.2f\n", p1.Distance(p2))
+}
